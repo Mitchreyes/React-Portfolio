c8567811f68e19c304d2f0f6c40967b3b5114815
Mitchreyes <mitchreyes@gmail.com>
2020-01-05 14:34:21 -0800

    J_03_building_contact_page_styles

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg"
 
 export default function() {
     return (
-        <div>
-            Contact
+        <div className="content-page-wrapper">
+            <div className="left-column"
+            style={{
+                background: "url(" + contactPagePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}>
+
+            </div>
+            <div className="right-column">
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="phone" />
+                        </div>
+
+                        <div className="text">
+                            808-393-3569
+                        </div>
+                    </div>
+                </div>
+
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="envelope" />
+                        </div>
+
+                        <div className="text">
+                            mitchreyes@gmail.com
+                        </div>
+                    </div>
+                </div>
+
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="map-marked-alt" />
+                        </div>
+
+                        <div className="text">
+                            Portland, OR
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
